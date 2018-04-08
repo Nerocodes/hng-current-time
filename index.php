1c71139a4064628cf50ec205a7d03f2b2bbfaf5e
nerocodes <neropaulej@gmail.com>
2018-04-08 23:16:45 +0100

    stage 1 task added to repo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HNG Stage 1</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Revalia|Roboto|Rokkitt" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>HNG</h1>
+        </header>
+        <main>
+            <div>
+                <h2>HNG Internship Stage 1</h2>
+                <p>The current time is: <?php date_default_timezone_set("Africa/Lagos"); echo date("h:ia"); ?></p>
+            </div>
+        </main>
+        <footer>
+            <p>NeroCodes &copy;2018</p>
+        </footer>
+    </body>
+</html>
